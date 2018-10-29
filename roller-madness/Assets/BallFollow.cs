137285a618e505e2733e39039914d66a5b089b68
Igormandello <igormandello@gmail.com>
2018-10-29 09:33:34 -0300

    Adds the scene elements

---
+++ roller-madness/Assets/BallFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFollow : MonoBehaviour {
+
+    private GameObject player, ground;
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        ground = GameObject.Find("Ground");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.RotateAround(Vector3.zero, Vector3.up, 2);
+        this.transform.LookAt(Vector3.zero);
+	}
+}
